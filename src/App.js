ac9f2a7ca701e6d5ad6db82ea82c03fc2a14d8ad
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-07 17:20:29 -0600

    initialized chatFeed component and prop

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import './App.css';
 import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
-<ChatEngine
-height='100vh' 
-projectID='
-7b350b1e-4fc6-4fcb-8f15-c51ba1f2e2e5'
-userName=''
-userSecret=''
+
+<div className='App'>
+
+<ChatEngine 
+height='100vh'
+projectID='7b350b1e-4fc6-4fcb-8f15-c51ba1f2e2e5'
+userName='jackxn'
+userSecret='2001'
+renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}
 />
-    </div>
+</div>
+
   );
+  
 }
 
 export default App;
+
+
+{/* <ChatEngine
+height='100vh' 
+projectID='
+7b350b1e-4fc6-4fcb-8f15-c51ba1f2e2e5'
+userName='jackxn'
+userSecret='2001'
+renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}
+/> */}
