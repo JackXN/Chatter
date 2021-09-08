5139956e2b18355dcf3fc4fee544fd070d823303
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-07 18:52:31 -0600

    Updated messageForm.jsx and imported react icons

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {sendMessage, isTyping} from 'react-chat-engine';
+import {AiOutlineSend as Send, AiOutlinePicture as Picture} from 'react-icons';
 
-function MessageForm() {
 
-const handleSubmit = () => {
 
+function MessageForm(props) {
+const [value, setValue] = useState('');
+const {chatId, creds} = props;
+
+const handleSubmit = (e) => {
+e.preventDefault();
+const text = value.trim();
+if(text.length > 0) sendMessage(creds,chatId,{ text });
+setValue('');
+}
+
+const handleChange = (e) => {
+    setValue(e.target.value);
+    isTyping(props, chatId);
 }
 
 
     return (
         <form className='message-form' onSubmit={handleSubmit}>
             <input
-            
+            className='message-input'
+            placeholder='Send a message...'
+            value={value}
+            onChange={handleChange}
+            onSubmit={handleSubmit} 
             />
+            <label htmlFor='upload-button'>
+                <span className='image-button'>
+
+                </span>
+            </label>
            
         </form>
     )
