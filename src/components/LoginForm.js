a70cd38c7d918a96246264afe659236345cbf9e6
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-07 22:00:57 -0600

    initialized required state

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,16 @@ import React, {useState} from 'react'
 import axios from 'axios';
 
 
+const projectID = '';
+
+
+
 function LoginForm() {
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('')
+const [error, setError] = useState('')
+
+
     return (
         <div>
             
