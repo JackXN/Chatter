6d7ff8223c8c0e73105df0596cf577bbe80ac194
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-07 22:04:42 -0600

    handleSubmit() ++

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -12,8 +12,21 @@ const [password, setPassword] = useState('')
 const [error, setError] = useState('')
 
 
+const handleSubmit = (e) => {
+    
+}
+
+
+
     return (
-        <div>
+        <div className='wrapper'>
+            <div className='form'>
+                <h1 className='title'>Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} className='input' placeholder='username' required/>
+                </form>
+            
+            </div>
             
         </div>
     )
